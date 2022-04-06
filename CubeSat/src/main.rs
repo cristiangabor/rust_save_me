def62e68da66ca92e0cf06dcde151df33a5b0518
Cristian Gabor <cristian.gabor@unifiedpost.com>
2022-04-06 23:16:30 +0300

    Next step

--- CubeSat/src/main.rs
+++ CubeSat/src/main.rs
@@ -1,4 +1,5 @@
-#![allow(unused_variables)]
+use std::ops::{Add};
+
 
 #[derive(Debug)]
 enum StatusMessage {
@@ -6,8 +7,16 @@ enum StatusMessage {
 }
 
 #[derive(Debug)]
+struct Event {
+    message: Vec<Message>,
+}
+
+type Message = String;
+
+#[derive(Debug)]
 struct TreeCount {
-    counter: u64,
+    counter_id: u64,
+    pub event: Event
 }
 
 fn check_status(counter: TreeCount) -> TreeCount {
@@ -16,11 +25,28 @@ fn check_status(counter: TreeCount) -> TreeCount {
     counter
 }
 
+fn add_with_lifetimes<'a, 'b>(i: &'a TreeCount, j: &'b TreeCount) -> u64 {
+    i.counter_id
+}
+
+fn add<T: std::ops::Add<Output = T>>(i: T, j: T) -> T {
+    i + j
+}
+
+struct Orchestrator;
+
+
+fn send(to: &mut TreeCount, msg: Message) {
+    to.event.message.push(msg);
+}
+
 fn main() {
     // Each satellite variable is represented by an integer.
-    let tree_count_a = TreeCount { counter: 0 };
-    let tree_count_b = TreeCount { counter: 1 };
-    let tree_count_c = TreeCount { counter: 2 };
+    Orchestrator {};
+
+    let tree_count_a = TreeCount { counter_id: 0, event: Event { message: vec![]}};
+    let tree_count_b = TreeCount { counter_id: 1, event: Event { message: vec![]}};
+    let tree_count_c = TreeCount { counter_id: 2, event: Event { message: vec![]}};
 
     let tree_count_a = check_status(tree_count_a);
     let tree_count_b = check_status(tree_count_b);
@@ -33,5 +59,14 @@ fn main() {
     let tree_count_b = check_status(tree_count_b);
     let tree_count_c = check_status(tree_count_c);
 
+
+    let res =  add_with_lifetimes(&tree_count_a, &tree_count_b);
+
+    
+
     println!("a: {:?}, b: {:?}, c: {:?}", tree_count_a, tree_count_b, tree_count_c);    
+
+    let floats = add(1.2, 3.4);
+
+    println!("{:?}, {}", res, floats);
 }
